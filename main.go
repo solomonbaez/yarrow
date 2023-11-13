a6d47fe34399dd525eef7472580807d4a37dd8fe
solomonbaez <solomon.james.baez@gmail.com>
2023-11-12 22:14:08 -0800

    Began implementing a simple file exploration method via hardcoded path

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+type File struct {
+	Name  string
+	IsDir bool
+}
+
+func ReadDir(path string) (files []File, err error) {
+	content, e := os.ReadDir(path)
+	if e != nil {
+		err = fmt.Errorf("error reading directory: %w", e)
+		log.Error().Err(err).Msg("")
+		return nil, err
+	}
+
+	for _, file := range content {
+		files = append(files, File{
+			Name:  file.Name(),
+			IsDir: file.IsDir(),
+		})
+	}
+
+	return
+}
 
 func main() {
-	fmt.Println(`Hello, world!`)
+	_, e := ReadDir(".")
+	if e != nil {
+		err := fmt.Errorf("fatal: %w", e)
+		log.Fatal().Err(err).Msg("")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Success!")
 }
